72eb4f66df7846ee5fb8a0165164f6c5194016b7
nassir_n <nassir_n@etna-alternance.net>
2019-06-23 19:09:50 +0200

    aggs with body

--- Flask-Restful/controller/aggs.py
+++ Flask-Restful/controller/aggs.py
@@ -7,13 +7,22 @@ from flask_restful import Resource, Api, reqparse, inputs
 from flaskext.mysql import MySQL
 
 class aggs(Resource):
+    def checkExist(a):
+        if not a:
+            a = None
+            return a
+        return a
     def get(self):
+        parser.add_argument('district', type=int, help='code district')
+        parser.add_argument('depart', type=str, help='code department')
+        district= checkExist(args['district'])
+        depart= checkExist(args['depart'])
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT * FROM towns')
+        cursor.execute('SELECT COUNT(Town_Name), MAX(Population), MIN(Population), AVG(Average_Age) FROM `towns` WHERE Code_District = %s OR Code_Department = %s', district, depart)
         rows = cursor.fetchall()
         resp = jsonify(rows)
         if not rows:
-            return jsonify({'about':'no towns found'})
+            return jsonify({'about':'no data found'})
         resp.status_code = 200
         return resp
