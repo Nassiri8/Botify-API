faacbd20e15ee3497047080f66d9e338af90e6df
nassir_n <nassir_n@etna-alternance.net>
2019-06-23 15:53:02 +0200

    route

--- Flask-Restful/api.py
+++ Flask-Restful/api.py
@@ -17,6 +17,10 @@ class Home(Resource):
 #Routes for home
 api.add_resource(Home, '/')
 api.add_resource(towns, '/towns')
+api.add_resource(TownByName, '/town/<string:name>')
+api.add_resource(deleteTown, '/town/delete/<string:name>')
+api.add_resource(townByRegion, '/town/region/<string:region>')
+api.add_resource(townWithMore, '/town/<int:population>')
 
 if __name__ == '__main__':
     app.run()
--- Flask-Restful/controller/town.py
+++ Flask-Restful/controller/town.py
@@ -19,21 +19,47 @@ class towns(Resource):
         return resp
 
 class deleteTown(Resource):
-    def delete(self, id):
+    def delete(self, name):
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('DELETE FROM bears WHERE id = %s', id)
+        cursor.execute('DELETE FROM towns WHERE Town_Name = %s', name)
         conn.commit()
-        return jsonify({'about':'bears delete'})
+        return jsonify({'about':'town delete'})
 
 class TownByName(Resource):
     def get(self, name):
+        name = '%'+name
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT * FROM towns WHERE name = %s', name)
+        cursor.execute('SELECT * FROM towns WHERE Town_Name LIKE %s', name)
         rows = cursor.fetchall()
         resp = jsonify(rows)
         if not rows:
             return jsonify({'about':'no town found'})
+        resp.status_code = 200
+        return resp
+
+class townByRegion(Resource):
+    def get(self, region):
+        region = '%'+region
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * FROM towns WHERE Region_Name LIKE %s', region)
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        if not rows:
+            return jsonify({'about':'no town found'})
+        resp.status_code = 200
+        return resp
+
+class townWithMore(Resource):
+    def get(self, population):
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * FROM `towns` WHERE Population > %s', population)
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        if not rows:
+            return jsonify({'about':'no town found with more than ' + population})
         resp.status_code = 200
         return resp
