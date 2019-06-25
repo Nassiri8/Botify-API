f9c6746877075cc08a344633e5808459c83dee17
nassir_n <nassir_n@etna-alternance.net>
2019-06-25 10:33:08 +0200

    create simple query -> select * from towns

--- Flask-Restful/controller/dsl.py
+++ Flask-Restful/controller/dsl.py
@@ -7,13 +7,21 @@ from flask_restful import Resource, Api, reqparse, inputs
 from flaskext.mysql import MySQL
 
 class traitement:
-    def fields(list):
+    def fields(self, list):
         if list:
             return True
         return False
     
-    def filters():
-        return True
+    def query(self, list):
+        i = 1
+        element = '{}'
+        if self.fields(list) == True:
+            nb = len(list)
+            while i <= nb:
+                query = 'SELECT'+element+' FROM towns'
+                i+=1
+        return query
+
 
 class dsl(Resource):
     def post(self):
@@ -21,7 +29,8 @@ class dsl(Resource):
         parser.add_argument('fields', type=list, location='json')
         parser.add_argument('filters', type=dict, location='json')
         args = parser.parse_args()
-        return args
+        t = traitement()
+        return t.query(args)
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         query = 'SELECT {}, {} FROM towns'.format(args['fields'][0], args['fields'][1])
